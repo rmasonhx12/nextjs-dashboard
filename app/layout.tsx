2c60063add46c4973ec0bcae327958c88e48278d
Robert Hendricks <rmasonh22@gmail.com>
2023-12-29 21:59:05 -0500

    initial next project

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+// add global css
+import '@/app/ui/gloqal.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
